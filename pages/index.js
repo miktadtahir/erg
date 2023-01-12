22b8ee466d381d4b74d8fcc3dd61d735177dc7d8
miktadtahir <miktadtahirdurak@gmail.com>
2023-01-12 05:22:02 +0300

    erg 3

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,21 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Erg Mühendislik</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="container">
+            <Head>
+                <title>Erg Mühendislik</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main>
-        <Header title="Erg Mühendislik - Yenileniyoruz!" />
-        <p className="description">
-         Çok Yakında <code>Burada</code>
-        </p>
-      </main>
+            <main>
+                <Header title="ERG - Yenileniyoruz!" />
+                <p className="description">
+                    Çok Yakında<code>Burada.</code>
+                </p>
+            </main>
 
-      <Footer />
-    </div>
-  )
+            <Footer />
+        </div>
+    )
 }
